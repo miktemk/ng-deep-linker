0f4bdc3f0ebb08fb9cf98cea17d5456ee9a8af3f
Mikhail Temkine <miktemk@gmail.com>
2014-03-14 19:33:21 -0400

    Can use several
    can use several ng-linkers to watch different groups of URL params

--- js/ng-deep-linker.js
+++ js/ng-deep-linker.js
@@ -1,8 +1,61 @@
-// Find this on GitHub https://github.com/miktemk/ng-deep-linker
+// From: https://github.com/mrharel/Object-Helper/blob/master/helper-object.js
+/**
+ * Compare two objects.
+ * @method isEqual
+ * @param o1 {Object} the object that is compared to
+ * @param o2 {Object} the object that is compared with
+ * @param cfg {Object} configuration settings:
+ * 	exclude {Object} key/val map where the key is properties names we want
+ * 	to exclude from the comparison. 
+ * 	strictMode [Boolean] if true then === will be used to compare premitive types, otherwise
+ * 	== will be used.
+ * 	noReverse {Boolean} if this is true then the method will avoid of the reverse comparison. 
+ * 		this will improve the efficiency and speed of the function but could cause to unwanted 
+ * 		results. For example, o1={a:1}; o2={a:1,b:2}; then if we compare o2 to o1 it will match
+ * 		because o2 has property 'a' that has the value 1, but if we compare o1 to o2 it will fail.
+ * 		this is why we are doing a reverse compare. but the price for the reverse is in performance, 
+ * 		since it is not implemented in the best way and it could reverse the same sub-objects more than 
+ * 		once. 
+ * @reverse - internal use of the recursion. do not use.
+ * @return {Boolean} true if the object are equal.
+ */
+$.isEqual = function (o1, o2, cfg, reverse) {
+	cfg = cfg || {};
+	cfg.exclude = cfg.exclude || {};
 
+	//first we check the reference. we don't care if null== undefined        
+	if (cfg.strictMode) {
+		if (o1 === o2) return true;
+	}
+	else {
+		if (o1 == o2) return true;
+	}
+
+	if (typeof o1 == "number" || typeof o1 == "string" || typeof o1 == "boolean" || !o1 ||
+		typeof o2 == "number" || typeof o2 == "string" || typeof o2 == "boolean" || !o2) {
+		return false;
+	}
+
+	if (((o1 instanceof Array) && !(o2 instanceof Array)) ||
+		((o2 instanceof Array) && !(o1 instanceof Array))) return false;
+
+	for (var p in o1) {
+		if (cfg.exclude[p] || !o1.hasOwnProperty(p)) continue;
+		if (!$.isEqual(o1[p], o2[p], cfg)) return false;
+	}
+	if (!reverse && !cfg.noReverse) {
+		reverse = true;
+		return $.isEqual(o2, o1, cfg, reverse);
+	}
+	return true;
+}
+
+// Find this on GitHub https://github.com/miktemk/ng-deep-linker
 function NgDeepLinker($location, $rootScope, $state) {
 	var self = this;
 	var fields = [];
+	var oldUrl = $location.path();
+	var oldParams = {}; // begin with empty to trigger callback on first call of checkUrlNow
 	var UrlUpdatingProgrammatically = false;
 
 	/// from obj to URL params
@@ -95,36 +148,66 @@ function NgDeepLinker($location, $rootScope, $state) {
 		return self;
 	};
 	self.checkUrlNow = function () {
-		var newObj = convertToObj($location.search());
-		if (self.onUrlUpdated_callback)
-			self.onUrlUpdated_callback(newObj);
+		// strip $location.search() down to just the fields we are interested in
+		var newParams = {};
+		var allParams = $location.search();
+		$.each(fields, function (i, field) {
+			newParams[field.urlName] = allParams[field.urlName];
+		});
+		// if changed, proceed
+		if (!$.isEqual(newParams, oldParams)) {
+			oldParams = newParams;
+			var newObj = convertToObj(newParams);
+			if (self.onUrlUpdated_callback)
+				self.onUrlUpdated_callback(newObj);
+		}
 		return self;
 	};
-
 	self.updateUrl = function (obj) {
-		// sanitize based on
+		// convert obj to URL parameters
 		var newParams = convertToParams(obj);
-		// compare objects
-		console.log("TODO: compare ", newParams, " and ", $state.params);
-		if (true) {
+		// if changed, proceed
+		if (!$.isEqual(newParams, oldParams)) {
+			oldParams = newParams;
+
+			// merge existing params with mine
+			var existingParams = $.extend({}, $location.search(), true);
+			$.each(fields, function (i, field) {
+				delete existingParams[field.urlName];
+			});
+			newParams = $.extend(existingParams, newParams, true);
+
+			// update query string
 			UrlUpdatingProgrammatically = true;
 			$location.search($.param(newParams));
 		}
-		//$location.path(urlBase).search(self.urlParam);
 		return self;
 	};
+	// maps just 1 field, by name
 	self.mapFieldFromParam = function (name) {
 		var q = $.grep(fields, function (x, i) { return x.name == name; });
 		if (q.length == 0)
 			return;
 		return _mapFieldToObj(q[0], $location.search());
-	}
+	};
+
+	// other utils
+	self.arrayFromUrlParam = function (name) {
+		var params = $location.search();
+		var sss = params[name];
+		if (!sss)
+			sss = '';
+		return sss.split(',');
+	};
 
 	$rootScope.$on("$locationChangeSuccess", function (event, current, previous) {
 		if (UrlUpdatingProgrammatically) {
 			UrlUpdatingProgrammatically = false;
 			return;
 		}
+		// we are not even on this page!
+		if ($location.path() != oldUrl)
+			return;
 		self.checkUrlNow();
 	});
 }
